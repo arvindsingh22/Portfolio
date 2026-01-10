534e63121307c9ce357301a633bb3e4730ea66d6
arvind singh <arvindrathorekct1234@gmail.com>
2026-01-10 21:35:15 +0530

    fix project image

--- src/data/constants.js
+++ src/data/constants.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/arvindsingh22",
   resume:
-    "https://drive.google.com/file/d/1SgGHDQbnVnyK3-hMIr2W-IXzwCLTvBOu/view",
+    "https://drive.google.com/drive/home",
   linkedin: "https://www.linkedin.com/in/arvind-singh-8104973a2/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/__arvind.rathore/",
